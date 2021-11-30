cafe45d271e1d829b24cc6de250af7a389153251
Rmariner25 <laxmi128644@gmail.com>
2021-11-30 19:21:39 +0530

    unwanted files eliminated

--- media/Files/palindrome_of_a_number.py
+++
@@ -1,9 +0,0 @@
-#To print palindrome of a number 
-x=int(input("Enter number:"))
-y=str(x)
-u=len(y)
-g=" "
-for i in range(-1,-(u+1),-1): #string backward indexing from -1 to -n
-    g=g+y[i] #string concatenation 
-print(g)
-    
